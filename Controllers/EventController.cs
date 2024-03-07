c3f8c453ab6a20d890a420fa708c97548bd3facb
Punnatat Rojekjit <pondpun123@gmail.com>
2024-03-07 22:00:13 +0700

    add to view

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -28,7 +28,7 @@ public class EventController : Controller
         string? user_id = HttpContext.Session.GetString("userID");
         if (user_id == null)
         {
-            return RedirectToAction("Login","User");
+            return RedirectToAction("Login", "User");
 
         }
         var user = await _userService.GetById(user_id);
@@ -36,7 +36,7 @@ public class EventController : Controller
             if (user == null)
             {
 
-                return RedirectToAction("Login","User");
+                return RedirectToAction("Login", "User");
 
             }
         }
@@ -53,7 +53,7 @@ public class EventController : Controller
         if (user_id == null)
         {
 
-            return RedirectToAction("Login","User");
+            return RedirectToAction("Login", "User");
         }
         newEvent.user_id = user_id;
         foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(newEvent))
@@ -98,7 +98,7 @@ public class EventController : Controller
         return View("Create");
     }
 
-    public async Task<IActionResult> EditEvent(string id)
+    public async Task<IActionResult> Edit(string id)
     {
         Event? _event = await _eventService.GetById(id);
         if (_event == null)
@@ -115,7 +115,8 @@ public class EventController : Controller
                 UserNoPassword user = await _userService.userProfile(participant.user_id);
                 pendingUser.Add(user);
 
-            } else if (participant.status == "submited")
+            }
+            else if (participant.status == "submited")
             {
                 submited_user++;
             }
@@ -140,11 +141,11 @@ public class EventController : Controller
             available_user = _event.max_member - submited_user,
             participants = pendingUser
         };
-        return Ok(editEvent);
+        return View(editEvent);
     }
 
-    public IActionResult Edit()
-    {
-        return View();
-    }
+    //     public IActionResult Edit()
+    //     {
+    //         return View();
+    //     }
 }
